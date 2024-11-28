03457da662680b6b409e0fdd510e65bc4898cd16
rahulgote4 <goterahul2000@gmail.com>
2024-11-28 14:33:55 +0530

    First commit

---
+++ Array.js
@@ -0,0 +1,53 @@
+   const myArr=[0,1,2,3,4]
+   const myHero=["ShaktimAN","Thor","IronMan","Caption America"]
+
+   const myArr2=new Array(1,2,3,4)      
+
+//    console.log(myArr);
+//    console.log(myHero);
+//    console.log(myArr2)
+
+//    console.log(myHero[1])
+
+
+
+   //Array Methods
+
+//    myArr.push(6)
+
+//    console.log(myArr)
+//    myArr.pop();
+//    console.log(myArr)
+
+
+// myArr.unshift(9);
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr)
+
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(9))
+
+// const newArry=myArr.join();
+//  console.log(myArr)
+//  console.log(newArry)
+
+//  console.log(typeof newArry)
+
+ //*********************************************** */
+ //Slice and Splice
+
+ console.log("A",myArr);
+
+ const myn1=myArr.slice(1,3)
+ console.log("myn1",myn1);
+
+ console.log("A",myArr);
+
+ const myn2=myArr.splice(1,3);
+ console.log("myn2",myn2);
+
+ console.log("A",myArr)
+
+
